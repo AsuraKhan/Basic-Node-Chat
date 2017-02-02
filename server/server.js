c4dbb4a2dc30bc6168f61a829910ce84c08ca255
Felipe Bezerra <felipeangells95@gmail.com>
2017-02-02 00:18:18 -0200

    some testing '

--- server/server.js
+++ server/server.js
@@ -22,11 +22,7 @@ io.on('connection', (socket) => {
 
 	socket.on('createMessage', (message) => {
 		console.log('createMessage', message);
-		io.emit('newMessage',{
-			from: message.from,
-			text: message.text,
-			createdAt: new Date().getTime()
-		});
+		io.emit('newMessage', message);
 	});
 
 	socket.on('disconnect', () => {
