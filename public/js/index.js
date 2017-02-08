35556f2cd60ebbb54a9dcc2aeaaeea0de41f1b3c
Felipe  Bezerra <felipeangells95@gmail.com>
2017-02-08 15:38:49 -0200

    update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -19,10 +19,14 @@ socket.on('newMessage', function(message) {
 
 	var li = $('<li></li>');
 	li.text(`${message.from}: ${message.text}`);
+
 	$('#messages').append(li);
     $('#messages').scrollTop( 99999 );
-
+    if(window.blur()){
+    	notifyMe(`${message.from}: ${message.text}`);  	
+    }
 });
+
 
 // socket.emit('createMessage', {
 // 		from: "Frank",
@@ -43,3 +47,32 @@ $('#message-form').on('submit', function(e){
 	});
 
 });
+
+function notifyMe(msg) {
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
+
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    var notification = new Notification(msg);
+    setTimeout(notification.close.bind(notification), 2000);
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        var notification = new Notification(msg);
+        setTimeout(notification.close.bind(notification), 2000);
+
+      }
+    });
+  }
+
+  // At last, if the user has denied notifications, and you 
+  // want to be respectful there is no need to bother them any more.
+}
